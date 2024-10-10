ab175f256ff4077cea6d2a8903acf43be5b9132c
renanguedes95 <renan.brega@viannasempre.com.br>
2024-10-10 07:58:51 -0300

    Início da criação do componente de informações sobre doações.

--- projeto-sao-camillo-lelis/src/App.js
+++ projeto-sao-camillo-lelis/src/App.js
@@ -1,3 +1,4 @@
+import InfoDoacoes from "./components/doacoes/InfoDoacoes";
 import Menu from "./components/menu/Menu";
 import SobreNos from "./components/SobreNos/SobreNos";
 import "./styles/styles.css";
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <div>
       <Menu />
+      <InfoDoacoes />
       <SobreNos />
     </div>
   );
---
+++ projeto-sao-camillo-lelis/src/components/doacoes/InfoDoacoes.js
@@ -0,0 +1,17 @@
+export default function InfoDoacoes() {
+  return (
+    <div className="container-doacoes">
+      <div className="container-doacoes-info">
+        <h1 className="container-doacoes-titulo">
+          Ajude a transformar vidas com a sua doação!
+        </h1>
+        <p className="container-doacoes-info">
+          Faça sua contribuição através dos dados bancários ao lado e participe
+          dessa causa solidária. Toda ajuda faz a diferença!
+        </p>
+      </div>
+
+      <img src="/placeholders/contaBancaria.jpg" />
+    </div>
+  );
+}
