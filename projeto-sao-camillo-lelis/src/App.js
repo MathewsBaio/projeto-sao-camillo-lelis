e6b214492cd6b0ec341202212ff40f4a6f26e15f
Mathews <baiomathews@gmail.com>
2024-10-10 13:26:35 -0300

    Componente nossos valores e correção de erros de responsividade

--- projeto-sao-camillo-lelis/src/App.js
+++ projeto-sao-camillo-lelis/src/App.js
@@ -2,6 +2,7 @@ import InfoDoacoes from "./components/doacoes/InfoDoacoes";
 import NossasHistorias from "./components/historias/NossasHistorias";
 import Menu from "./components/menu/Menu";
 import SobreNos from "./components/SobreNos/SobreNos";
+import NossosValores from "./components/valores/NossosValores";
 import "./styles/styles.css";
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Menu />
       <InfoDoacoes />
       <SobreNos />
+      <NossosValores />
       <NossasHistorias />
     </div>
   );
---
+++ projeto-sao-camillo-lelis/src/components/valores/NossosValores.js
@@ -0,0 +1,40 @@
+export default function NossosValores() {
+  return (
+    <div className="container-valores">
+      <h2>NOS AJUDE A TRANSFORMAR VIDAS</h2>
+      <h2>Nossos Valores</h2>
+      <div className="valores">
+        <div className="valor">
+          <h3>#VALOR</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+        <div className="valor">
+          <h3>#VALOR</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+        <div className="valor">
+          <h3>#VALOR</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
