73f819602639484c8f9aefbc6a147bd606a2f712
Mathews <baiomathews@gmail.com>
2024-12-06 10:59:56 -0300

    Correção do href de programas do footer

--- projeto-sao-camillo-lelis/src/components/footer/FooterNav.js
+++ projeto-sao-camillo-lelis/src/components/footer/FooterNav.js
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
 export default function FooterNav() {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="footer-list">
       <dl>
@@ -7,11 +23,19 @@ export default function FooterNav() {
             Sobre nós
           </a>
         </li>
-        <li>
-          <a className="btn" href="#programas">
-            Programas
-          </a>
-        </li>
+        {isMobile ? (
+          <li>
+            <a className="btn" href="#programas-mobile">
+              Programas
+            </a>
+          </li>
+        ) : (
+          <li>
+            <a className="btn" href="#programas">
+              Programas
+            </a>
+          </li>
+        )}
         <li>
           <a className="btn" href="#historias">
             Nossas Histórias
